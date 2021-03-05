dde19f117ef03e262d3b4abd7fc0bbfb08ff4ee9
sweetbabyjay <jayfly64@gmail.com>
2021-03-05 08:55:18 -0800

    Updates

---
+++ precache-manifest.0e56cd4ecf95c7b632457c9034d0854c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5554d202513eeaba573d",
+    "url": "/jd-city-council/static/css/main.2deeacbc.chunk.css"
+  },
+  {
+    "revision": "5554d202513eeaba573d",
+    "url": "/jd-city-council/static/js/main.5554d202.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/jd-city-council/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "140bad3a45d889f64041",
+    "url": "/jd-city-council/static/js/runtime~main.140bad3a.js"
+  },
+  {
+    "revision": "15a82b5383e653866424845e76030d7c",
+    "url": "/jd-city-council/index.html"
+  }
+];
